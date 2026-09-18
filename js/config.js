692cbc4425a6423327e3191ab2e809ba579b2196
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-18 16:33:21 -0300

    Centraliza configuración de invitación Spider-Man

---
+++ js/config.js
@@ -0,0 +1,47 @@
+window.INVITATION_CONFIG = {
+  invitation: {
+    name: "Mariano",
+    age: 8,
+    message: "Te invito a que pasemos una tarde muy divertida celebrando mi cumpleaños.",
+    eventDateTime: "2026-10-10T16:00:00-03:00",
+    eventEndDateTime: "2026-10-10T20:00:00-03:00",
+    dateLabel: "Sábado 10 de Octubre",
+    timeLabel: "16:00 a 20:00 hs.",
+    venue: "Salón de Fiestas Cirilo",
+    address: "Álvarez Thomas 1320",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=Sal%C3%B3n+de+Fiestas+Cirilo+%C3%81lvarez+Thomas+1320+Buenos+Aires",
+    whatsappNumber: "5491156223007",
+    whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Mariano. 🕷🕸",
+    instagramUrl: "https://www.instagram.com/rossdigitalstudio/",
+    instagramHandle: "@rossdigitalstudio",
+    audioUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionSpiderMan/main/www.tarjetas.cool/modelos/cumple/spiderman/sounds/audio3860.mp3",
+    portraitUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionSpiderMan/main/www.tarjetas.cool/modelos/cumple/spiderman/img/foto1.jpg",
+    ribbonUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionSpiderMan/main/www.tarjetas.cool/modelos/cumple/spiderman/img/cinta.png"
+  },
+  characters: [
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionSpiderMan/main/www.tarjetas.cool/modelos/cumple/spiderman/img/1.png",
+      width: "min(92vw, 420px)",
+      top: "24px",
+      translateX: "-52%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionSpiderMan/main/www.tarjetas.cool/modelos/cumple/spiderman/img/4.png",
+      width: "min(116vw, 500px)",
+      top: "-48px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionSpiderMan/main/www.tarjetas.cool/modelos/cumple/spiderman/img/3.png",
+      width: "min(92vw, 420px)",
+      top: "2px",
+      translateX: "-49%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionSpiderMan/main/www.tarjetas.cool/modelos/cumple/spiderman/img/2.png",
+      width: "min(98vw, 440px)",
+      top: "10px",
+      translateX: "-50%"
+    }
+  ]
+};
